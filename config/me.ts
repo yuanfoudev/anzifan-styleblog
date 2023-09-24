9768d55a12365df210b437b51f39593923c51a49
arc-designing <145824138+arc-designing@users.noreply.github.com>
2023-09-24 11:29:20 +0800

    Update me.ts

--- config/me.ts
+++ config/me.ts
@@ -2,7 +2,7 @@ import { links } from '../config/links'
 import { Gmail, Java, Python , Javascript, Typescript, Swift, Mysql, Realm, Mongodb, Html5, CssThree, ReactJs, Nodedotjs, Nextdotjs, Vuedotjs, Tailwindcss, Spring, Springboot, Stylus , Scikitlearn, Tensorflow, Docker, Git, Redis, Apachehadoop, Postman} from '@icons-pack/react-simple-icons'
 
 export const me = {
-    site: "https://www.anzifan.com",
+    site: "https://sjbzh.ac.cn",
     name: '设计标准化💯',
     bio: '💻 🎨 🎮 ⚡️',
     social: [
