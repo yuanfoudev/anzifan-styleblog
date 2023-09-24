e577945be84fd1baf3a0206677ac8c285c7ff6ff
arc-designing <145824138+arc-designing@users.noreply.github.com>
2023-09-24 11:37:58 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -61,8 +61,8 @@ function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
     <ThemeProvider attribute="class">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <title>安子璠的个人主页</title>
-        <meta name="description" content="异次元の机智君" />
+        <title>设计标准化的个人主页</title>
+        <meta name="description" content="设计标准化" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
