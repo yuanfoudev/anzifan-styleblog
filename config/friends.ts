cc05da218758c3701716cea0a0cd921bf1ec30f6
arc-designing <145824138+arc-designing@users.noreply.github.com>
2023-10-14 16:51:20 +0800

    Update friends.ts

--- config/friends.ts
+++ config/friends.ts
@@ -6,6 +6,11 @@ export type FriendType = {
 
 export const friends: FriendType[] = [
   {
+    url: 'https://blog.ikk.ink',
+    name: 'TeleGalaxy(欢迎访问)',
+    img: 'https://www.fomal.cc/assets/head.jpg',
+  },
+  {
     url: 'https://www.xiaohongshu.com/user/profile/611b7ce10000000001015a52?xhsshare=CopyLink&appuid=62a4b19e000000002102932e&apptime=1695618050',
     name: '设计标准化',
     img: 'https://cdn.yhz610.com/pic/blog/zhuazi.svg',
