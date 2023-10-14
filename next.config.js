5863a8add21b1ed428ed2f093c4c049a94c2917e
arc-designing <145824138+arc-designing@users.noreply.github.com>
2023-10-14 13:35:11 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = withPWA(
       return config
     },
     images: {
-      domains: ['www.notion.so/sjbzh/Test-4c42101985b34bc497179e49f5e7a5cf?pvs=4','cdn.yhz610.com', 'images.adsttc.com', 'www.notion.so/sjbzh/57a9d86d94e84ae0baae7517f5535147?v=7f3dc97af0764a8187b83c5cd869bab7&pvs=4','cdn.yhz610.com','static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'image.cugxuan.cn', 'blog-static.mikuchan.top', 'amazonaws.com', 'img.zhheo.com', 'www.aohuiliu.fun', 'rxhsk.xicp.fun', 'www.fomal.cc'],
+      domains: ['www.notion.so/sjbzh/Test-4c42101985b34bc497179e49f5e7a5cf?pvs=4','images.unsplash.com','cdn.yhz610.com', 'images.adsttc.com', 'www.notion.so/sjbzh/57a9d86d94e84ae0baae7517f5535147?v=7f3dc97af0764a8187b83c5cd869bab7&pvs=4','cdn.yhz610.com','static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'image.cugxuan.cn', 'blog-static.mikuchan.top', 'amazonaws.com', 'img.zhheo.com', 'www.aohuiliu.fun', 'rxhsk.xicp.fun', 'www.fomal.cc'],
     },
     pwa: {
       dest: "public",
