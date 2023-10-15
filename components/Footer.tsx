5325bb3229f8b01778ec912536aa50181cb821e9
arc-designing <145824138+arc-designing@users.noreply.github.com>
2023-10-15 19:38:43 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -46,10 +46,10 @@ const Footer = () => {
                                         )}
                                     </div>
                                 </div>
-                                <p className="group">Design & Build with <span className="group-hover:text-red-400 transition-colors duration-500 ease-in-out">❤</span> by <a className="text-true-gray-600 hover:underline" href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5NTkwMDAzOA==" target="_blank" rel="noopener noreferrer" dark="text-true-gray-400">MannyCooper</a></p>
+                                <p className="group">Design & Build with <span className="group-hover:text-red-400 transition-colors duration-500 ease-in-out">❤</span> by <a className="text-true-gray-600 hover:underline" href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5NTkwMDAzOA==" target="_blank" rel="noopener noreferrer" dark="text-true-gray-400">设计标准化</a></p>
                             </div>
                             <p className="flex flex-col items-end">
-                                <a className="hover:underline" href="https://anzifan-old.vercel.app" target="_blank" rel="noopener noreferrer">Old Blog</a>
+                                <a className="hover:underline" href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5NTkwMDAzOA==" target="_blank" rel="noopener noreferrer">公众号</a>
                             </p>
                         </div>
                         <div className="text-xs flex justify-between">
@@ -58,7 +58,7 @@ const Footer = () => {
                                     {`Copyright © 2020-${thisYear} `}
                                     <Link href={"/me"}>
                                         <a className="text-true-gray-600 hover:underline" dark="text-true-gray-400">
-                                            安子璠
+                                            设计标准化
                                         </a>
                                     </Link>
                                 </span>                                
@@ -66,9 +66,9 @@ const Footer = () => {
                             <span><a className="text-true-gray-600 hover:underline" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer" dark="text-true-gray-400">CC BY-NC-SA 4.0</a></span>
                         </div>
                         <p className="space-x-1 md:space-x-2 text-[10px] mt-2 text-true-gray-300">
-                                    <span><a className="hover:underline hover:text-true-gray-400" dark="text-true-gray-700 hover:text-true-gray-400" href="http://beian.miit.gov.cn/" rel="noopener noreferrer" target="_blank">苏ICP备 2020060340 号</a></span>
-                                    <span><a className="hover:underline hover:text-true-gray-400" dark="text-true-gray-700 hover:text-true-gray-400" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32011202000528" rel="noopener noreferrer" target="_blank">苏公网安备 32011202000528 号</a></span>
-                                </p>
+                                    <span><a className="hover:underline hover:text-true-gray-400" dark="text-true-gray-700 hover:text-true-gray-400" href="http://beian.miit.gov.cn/" rel="noopener noreferrer" target="_blank">鲁ICP备2023031946号</a></span>
+{/*                                       <span><a className="hover:underline hover:text-true-gray-400" dark="text-true-gray-700 hover:text-true-gray-400" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32011202000528" rel="noopener noreferrer" target="_blank">苏公网安备 32011202000528 号</a></span> */}
+                        </p>  
                     </section>
                 </div>
             </footer>
