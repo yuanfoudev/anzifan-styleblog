d4cb5f3288beb7fa678f86ecd3ffb868f0e7ab8e
arc-designing <145824138+arc-designing@users.noreply.github.com>
2023-10-15 10:52:23 +0800

    Update me.tsx

--- pages/me.tsx
+++ pages/me.tsx
@@ -84,7 +84,7 @@ const Overview = () => {
                     Intersted in</p>
                 <div className="text-center">
                     <p className="text-sm font-semibold xs:text-xl sm:text-2xl md:text-3xl">
-                        Design, Drawing, Reading, Architectural Model, Film, Basketball, Music, Travelling, 
+                        Design, Drawing, Architectural Model, Reading, Basketball, Travelling, Web & Mobile Development
                     </p>
 
                 </div>
